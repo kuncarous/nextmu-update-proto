c359676730b7198ee11bf80718c42b870e7eccfa
kuncarous <mscsirmaster@gmail.com>
2024-04-27 17:59:27 +0200

    modified to use @grpc/proto-loader fixed enum naming

--- proto-generator.ts
+++ proto-generator.ts
@@ -24,25 +24,16 @@ process.env.PATH += path.delimiter + path.resolve('./node_modules/.bin');
 
 const PROTO_DIR = path.resolve('./proto/models');
 const MODEL_DIR = path.resolve(process.env.PROTO_PATH!);
-const PROTOC_GEN_TS_PATH = path.resolve(
-    './node_modules/.bin/protoc-gen-ts.cmd',
-);
 
 rimrafSync(MODEL_DIR);
 try {
     mkdirSync(MODEL_DIR, { recursive: true });
 } catch (error) {}
 
-const protoConfig = [
-    `--plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}"`,
-    `--grpc_out="grpc_js:${MODEL_DIR}"`,
-    `--js_out="import_style=commonjs,binary:${MODEL_DIR}"`,
-    `--ts_out="grpc_js:${MODEL_DIR}"`,
-    `--proto_path ${PROTO_DIR} ${getAllFiles(PROTO_DIR).join(' ')}`,
-];
-
 // https://github.com/agreatfool/grpc_tools_node_protoc_ts/tree/master/examples
-shell.exec(`grpc_tools_node_protoc ${protoConfig.join(' ')}`);
+shell.exec(
+    `proto-loader-gen-types --longs=String --enums=String --defaults --oneofs --grpcLib=@grpc/grpc-js --outDir=${path.join(MODEL_DIR, path.sep)} ${getAllFiles(PROTO_DIR).join(' ')}`,
+);
 
 // https://github.com/dcodeIO/protobuf.js#command-line
 // https://github.com/dcodeIO/protobuf.js#command-line-api
